8fe760b9e9c39396fb84ade9faa9ee20931ff149
leodrumers <leodrumers@gmail.com>
2020-09-15 19:24:12 -0500

    Creando modelos para manejar bandas Creando funciones para manejar el CRUD de las bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,19 @@
 const { io } = require('../index.js');
+const Bands = require('../models/bands.js');
+const Band = require('../models/band.js');
+
+const bands = new Bands();
+bands.addBand(new Band('Switchfoot'));
+bands.addBand(new Band('Skillet'));
+bands.addBand(new Band('Israel Houghton'));
+bands.addBand(new Band('Hillsong'));
+bands.addBand(new Band('Marco Barrientos'));
 
 // Mensajes de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -18,5 +29,24 @@ io.on('connection', client => {
         //io.emit('nuevo-mensaje', payload)
         client.broadcast.emit('nuevo-mensaje', payload);
         console.log('mensaje ', payload);
+    });
+
+    client.on('vote-band', (payload) => {
+        console.log(payload)
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        console.log(payload)
+        bands.addBand(newBand)
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        console.log('deleting', payload)
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
     });
 });
